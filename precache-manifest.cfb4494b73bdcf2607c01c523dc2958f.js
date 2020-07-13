df52eda81aafee48d0d7bcb2b1d35e1f51afcc2e
Vadim <unistars@mail.ru>
2020-07-13 11:52:20 +0300

    Updates

---
+++ precache-manifest.cfb4494b73bdcf2607c01c523dc2958f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef49ead5c5e70a5a057f1527a27efa09",
+    "url": "/BrainForce/index.html"
+  },
+  {
+    "revision": "6c0565979486dd3d893a",
+    "url": "/BrainForce/static/css/main.be9c10d4.chunk.css"
+  },
+  {
+    "revision": "7405553ce8394ab2c4b5",
+    "url": "/BrainForce/static/js/2.836f8dea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BrainForce/static/js/2.836f8dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c0565979486dd3d893a",
+    "url": "/BrainForce/static/js/main.beb63a92.chunk.js"
+  },
+  {
+    "revision": "af22569b08c35b5f33aa",
+    "url": "/BrainForce/static/js/runtime-main.d2c81aae.js"
+  },
+  {
+    "revision": "69f40e5417c4e3804ff2b6b9d5fb96b2",
+    "url": "/BrainForce/static/media/Subtract.69f40e54.png"
+  },
+  {
+    "revision": "dc4679e9708e8f43ad74dc969095952e",
+    "url": "/BrainForce/static/media/banner.dc4679e9.png"
+  },
+  {
+    "revision": "9c9bac17795e07ac01e92238c32b5921",
+    "url": "/BrainForce/static/media/element2.9c9bac17.png"
+  },
+  {
+    "revision": "ba9e71b7ff3b3b7fff163a7959c8e703",
+    "url": "/BrainForce/static/media/element3.ba9e71b7.png"
+  },
+  {
+    "revision": "d2f5b23c13c395ead9b9c9c24c59ebcc",
+    "url": "/BrainForce/static/media/imageBlock3.d2f5b23c.png"
+  },
+  {
+    "revision": "92af67102b3e862996a6fdc5cdecc44b",
+    "url": "/BrainForce/static/media/messages.92af6710.png"
+  },
+  {
+    "revision": "f8320362a3087cd7a7e44eb022262bde",
+    "url": "/BrainForce/static/media/pictures.f8320362.png"
+  },
+  {
+    "revision": "cf10313c86d3c22c69938c7efc29e224",
+    "url": "/BrainForce/static/media/union.cf10313c.png"
+  }
+]);
